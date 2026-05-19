4c49514a3948bb0c904d62d3d8ccc852d31ef9bc
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-19 08:35:05 -0300

    Model do usuário aperfeiçoado

--- backend/src/models/Usuario.js
+++ backend/src/models/Usuario.js
@@ -2,10 +2,10 @@ import { Schema, model } from "mongoose"
 
 const SchemaUsuario = Schema({
   nome: { type: String, required: true},
-  email: { type: String, required: true},
+  email: { type: String, required: true, unique: true},
   telefone: { type: String, required: true},
   senha: { type: String, required: true},
-  hole: { type: String, required: true, default: "user"} // user e admin
+  role: { type: String, enum: ["user", "admin"], default: "user"}
 })
 
 const ModelUsuario = model("usuario", SchemaUsuario)
