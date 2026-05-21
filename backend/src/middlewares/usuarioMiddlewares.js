c5e32ecf97884d89a467395a14899bd66aedc206
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-21 11:27:53 -0300

    Middleware de checagem de de admin adicionado

--- backend/src/middlewares/usuarioMiddlewares.js
+++ backend/src/middlewares/usuarioMiddlewares.js
@@ -24,3 +24,19 @@ export async function checarToken(request, response, next) {
     return response.status(401).json({ Erro: "Erro ao checar o Token!" })
   }
 }
+
+export async function checarAdmin(request, response, next) {
+  try {
+    const token = request.usuario
+
+    if(token.role != "admin"){
+      return response.status(403).json({ Erro: "Usuário não autorizado!" })
+    }
+
+    next()
+
+  } catch(erro) {
+    console.log(erro)
+    return response.status(403).json({ Erro: "usuário não autorizado!" })
+  }
+}
