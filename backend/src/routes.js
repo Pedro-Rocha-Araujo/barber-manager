dba7e32f7c4bf7333c7c39eab514670d13c795d3
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-21 14:49:07 -0300

    Rotas configuradas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,23 @@
 import { Router } from "express"
+// imports de Usuário / Seção
 import {
   listarUsuarios,
   cadastroUsuario,
   loginUsuario
 } from "./controllers/usuarioController.js"
-
+// Imports de Cortes
 import {
   listarCortes,
-  listarCorte
+  listarCorte,
+  cadastrarCorte,
+  editarCorte,
+  deletarCorte
 } from "./controllers/cortesController.js"
+// imports dos Middleares
+import {
+  checarToken,
+  checarAdmin
+} from "./middlewares/usuarioMiddlewares.js"
 
 const router = Router()
 
@@ -17,7 +26,10 @@ router.get("/usuarios", listarUsuarios)
 router.post("/cadastro", cadastroUsuario)
 router.post("/login", loginUsuario)
 // Rotas de Corte
-router.get("/cortes", listarCortes)
-router.get("/corte/:id", listarCorte)
+router.get("/cortes", checarToken, listarCortes)
+router.get("/corte/:id", checarToken, listarCorte)
+router.post("/novo-corte", checarToken, checarAdmin, cadastrarCorte)
+router.put("/editar-corte/:id", checarToken, checarAdmin, editarCorte)
+router.delete("/deletar-corte/:id", checarToken, checarAdmin, deletarCorte)
 
 export default router
