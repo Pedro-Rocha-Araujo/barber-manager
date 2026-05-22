34c48ae2119a48571b94336e5d526cbbd03070dc
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-22 10:25:31 -0300

    Card implementado e CRUD dos cortes funcionando no front e back

--- frontend/src/components/cortes/ListagemCortes.js
+++ frontend/src/components/cortes/ListagemCortes.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react"
+import { toast } from "react-toastify"
 import axios from "axios"
+import CardCorte from "../elements/CardCorte"
 
 function ListagemCortes() {
+
   const [cortes, setCortes] = useState([])
+  const [card, setCard] = useState(null)
 
   useEffect(()=>{
     async function getCortes() {
@@ -16,16 +20,33 @@ function ListagemCortes() {
     getCortes()
   }, [])
 
+  function acionarCard(id) {
+    try {
+      setCard(id)
+    } catch(erro) {
+      console.log(erro)
+      setCard(null)
+      toast.error("Erro!")
+    }
+  }
+
   return (
     <section className="listagem-cortes">
+
+      { card && (
+        <CardCorte id={card} setCard={setCard} />
+      ) }
+
       <h2>Cortes Disponíveis</h2>
       <div className="cortes">
 
         {cortes.map((corte, index)=>{
           return (
-            <div key={corte._id} className="corte">
-              <h3>{corte.nome}</h3>
-              <i className="fa-solid fa-eye fa-xl"></i>
+            <div onClick={()=>acionarCard(corte._id)} key={corte._id} className="corte">
+
+                <h3>{`${corte.nome} - R$${corte.preco}`}</h3>
+                <i className="fa-solid fa-eye fa-xl"></i>
+
             </div>
           )
         })}
---
+++ frontend/src/components/elements/CardCorte.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { toast } from "react-toastify"
+import "./card.css"
+
+function CardCorte({ id, setCard }) {
+  const navigate = useNavigate()
+  const token = localStorage.getItem("token")
+  const [itemCard, setItemCard] = useState()
+
+  useEffect(()=>{
+    async function getItemCard(id) {
+      try {
+        const response = await axios.get(`http://localhost:4000/cortes/${id}`)
+        setItemCard(response.data)
+      } catch(erro) {
+        console.log(erro)
+      }
+    }
+    getItemCard(id)
+  }, [itemCard, id])
+
+  function editarCorte(id) {
+    try {
+      navigate(`/editar-corte/${id}`)
+    } catch(erro) {
+      console.log(erro)
+      toast.error("Erro!")
+    }
+  }
+
+  async function deletarCorte(id) {
+    try {
+      await axios.delete(`http://localhost:4000/cortes/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      navigate(0)
+    } catch(erro) {
+      console.log(erro)
+      toast.error("Erro!")
+    }
+  }
+
+  function fecharCard() {
+    setCard(null)
+  }
+
+  return (
+    <div className="background">
+      <div className="card">
+        <div className="dois">
+          <h1>Corte: { itemCard?.nome }</h1>
+          <i onClick={()=>deletarCorte(itemCard._id)} className="fa-solid fa-trash fa-xl"></i>
+        </div>
+          <p><strong>Preço:</strong> R${itemCard?.preco}</p>
+          <p><strong>Descrição:</strong> { itemCard?.descricao }</p>
+          <button onClick={()=>editarCorte(itemCard._id)} className="blue">Editar</button>
+          <button onClick={fecharCard} className="red">Fechar</button>
+      </div>
+    </div>
+  )
+}
+
+export default CardCorte
