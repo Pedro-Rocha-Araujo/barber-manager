6cf63ef6fe12998b9d11b552f16994cd63df5c3e
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-01 09:16:38 -0300

    Readme

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+
 // imports de Usuário / Seção
 import {
   listarUsuarios,
@@ -6,6 +7,7 @@ import {
   loginUsuario,
   editarPermissao
 } from "./controllers/usuarioController.js"
+
 // Imports de Cortes
 import {
   listarCortes,
@@ -14,12 +16,14 @@ import {
   editarCorte,
   deletarCorte
 } from "./controllers/cortesController.js"
+
 // Imports de Agendamento
 import {
   listarAgendamentos,
   agendarCorte,
   listarHorarios
 } from "./controllers/agendamentoController.js"
+
 // imports de Perfil / Dashboard
 import {
   cortesDoDia,
@@ -27,6 +31,7 @@ import {
   cancelarAgendamento,
   desmarcarAgendamento
 } from "./controllers/perfilController.js"
+
 // imports dos Middleares
 import {
   checarToken,
@@ -40,16 +45,19 @@ router.get("/usuarios", listarUsuarios)
 router.post("/cadastro", cadastroUsuario)
 router.post("/login", loginUsuario)
 router.put("/editar-permissao/:id", editarPermissao)
+
 // Rotas de Corte
 router.get("/cortes", listarCortes)
 router.get("/cortes/:id", listarCorte)
 router.post("/cortes", checarToken, checarAdmin, cadastrarCorte)
 router.put("/cortes/:id", checarToken, checarAdmin, editarCorte)
 router.delete("/cortes/:id", checarToken, checarAdmin, deletarCorte)
+
 // Rotas de Agendamento
 router.get("/agendamentos", listarAgendamentos)
 router.get("/agendamentos/:data", listarHorarios)
 router.post("/agendamentos", checarToken, agendarCorte)
+
 // Rotas de Perfil / Dashboard
 router.get("/cortes-do-dia", checarToken, checarAdmin, cortesDoDia)
 router.get("/meus-cortes", checarToken, meusCortes)
